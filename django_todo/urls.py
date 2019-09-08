48f8e9448234004ce938f3e8d0cfb18e930fe909
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-08 22:00:16 +0100

    Created and tested working edit button

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list, create_an_item
+from todo.views import get_todo_list, create_an_item, edit_item
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', get_todo_list),
     path('add', create_an_item),
+    path('<int:item_id>', edit_item),
 ]
