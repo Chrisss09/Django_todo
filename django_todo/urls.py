719dbc86e4cf403d7d7d2961e9bc7cbe5504e23b
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-08 14:30:38 +0100

    Creating a form

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list
+from todo.views import get_todo_list, create_an_item
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', get_todo_list),
+    path('add', create_an_item),
 ]
