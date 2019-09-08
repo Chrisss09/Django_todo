6b7227cba4c9037084ba333dc7b832e584e14710
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-08 09:32:45 +0100

    Initial commit

---
+++ django_todo/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from todo.views import get_todo_list
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', get_todo_list),
+]
