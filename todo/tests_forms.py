5172e91c093a1ad3d374469d2c152a640ffa7c67
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-09 20:17:20 +0100

    Added testing to project

---
+++ todo/tests_forms.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from .forms import ItemForm
+
+class TestToDoItemForm(TestCase):
+
+    def test_create_item_with_name(self):
+        form = ItemForm({'name': 'Create Tests'})
+        self.assertTrue(form.is_valid())
+
+    def test_correct_message_for_missing_name(self):
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['name'], [u'This field is required.'])
