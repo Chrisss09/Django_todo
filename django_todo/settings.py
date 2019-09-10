176fe8ffc2413c7c569b9d7d462d73d808c6aaf2
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-10 20:44:02 +0100

    Added host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = ['chris-todo-siteapp.herokuapp.com']
 
 
 # Application definition
