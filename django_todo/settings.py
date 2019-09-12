83a616d40a284449f42e80f7c5b1cc341b66ef1d
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-12 19:26:16 +0100

    Changed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME'), os.environ.get('LOCALHOST')]
+ALLOWED_HOSTS = [os.environ.get('HOSTNAME'), '127.0.0.1']
 
 
 # Application definition
